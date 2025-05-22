b213c699a48c978bfb5358ee1d3c2b4e9a3a46d2
jplwunder <jplwunder@gmail.com>
2025-05-22 18:14:26 -0300

    refactor: remove customer handlers and update routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,7 +13,8 @@ func (app *application) wrapHandler(h http.HandlerFunc) httprouter.Handle {
 		for _, param := range ps {
 			paramsMap[param.Key] = param.Value
 		}
-
+		type contextKey string
+		const httprouterParamsKey = contextKey("httprouterParams")
 		ctx := context.WithValue(r.Context(), httprouterParamsKey, paramsMap)
 		r = r.WithContext(ctx)
 
@@ -28,9 +29,6 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.GET("/healthcheck", app.wrapHandler(app.healthcheckHandler))
-	router.GET("/customers", app.wrapHandler(app.listCustomersHandler))
-	router.POST("/customers", app.wrapHandler(app.createCustomerHandler))
-	router.GET("/customers/:id", app.wrapHandler(app.getCustomerHandler))
 
 	return app.recoverPanic(app.rateLimit(app.enableCORS(router)))
 }
